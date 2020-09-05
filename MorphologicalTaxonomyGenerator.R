f093a49ecb5d406786f3f83be3ce5968ee416e41
Ariel Levi Simons <levisimons@gmail.com>
2020-09-05 16:54:28 -0700

    Update MorphologicalTaxonomyGenerator.R

--- MorphologicalTaxonomyGenerator.R
+++ MorphologicalTaxonomyGenerator.R
@@ -9,10 +9,30 @@ require(taxize)
 options(ENTREZ_KEY="3f9e29c7c03842f72cf7523e34390d9f2208")
 wd <- "~/Desktop/SCCWRP/MorphologicalBMIsAlgae/"
 setwd(wd)
+#This script generates full taxonomies for morphologically classified stream communities from SCCWRP.
 
-#Read in morphological stream community data and format them as presence/absence tables.
+#Read in morphological stream community data describing all algae (soft algae and diatoms)
+#and format them as presence/absence tables.
 AlgalInput <- read.table("AlgTaxa.csv", header=T, sep=",",as.is=T,skip=0,fill=T,quote="\"",check.names=F,encoding = "UTF-8")
+#Standarize date format.
+AlgalInput$sampledate <- as.Date(AlgalInput$sampledate,format="%Y-%m-%d")
+AlgalInput$sampledate <- format(AlgalInput$sampledate,format="%m/%d/%y")
+#Create unique sample identifier.
+AlgalInput$UniqueID <- paste(AlgalInput$stationcode,AlgalInput$sampledate)
 
+#Read in table linking sample IDs in the metagenomic table to sample station codes.
+#This will filter the morphological data to containing the same sample sets.
+sampleIDs <- read.table("SampleStationCodesID.txt", header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,check.names=FALSE, encoding = "UTF-8")
+#Remove dubious samples
+sampleIDs <- sampleIDs[which(sampleIDs$SampleNum!=202 & sampleIDs$SampleNum!=230),]
+sampleIDs$Date <- as.Date(sampleIDs$Date,format="%m/%d/%y")
+sampleIDs$Date <- format(sampleIDs$Date,format="%m/%d/%y")
+#Create unique sample identifier.
+sampleIDs$UniqueID <- paste(sampleIDs$StationCode,sampleIDs$Date)
+
+#Filter morphological algal data to containing the same samples as the metagenomic ones.
+AlgalInput <- AlgalInput[AlgalInput$UniqueID %in% sampleIDs$UniqueID,]
+  
 #Get a list of unique OTU names from count tables and convert to a data frame.
 uniqueOTUs <- as.data.frame(unique(AlgalInput$finalid))
 colnames(uniqueOTUs) <- c("FinalID")
@@ -71,8 +91,19 @@ AlgalTaxonomies[] <- lapply(AlgalTaxonomies,as.character)
 #Write out algal taxonomy data.
 write.table(AlgalTaxonomies,"AlgalTaxonomiesMorphological.txt",quote=FALSE,sep="\t",row.names = FALSE)
 
-#Read in morphological stream community data and format them as presence/absence tables.
+#Generate full taxonomies for BMIs.
+#Read in morphological stream community data for BMIS and format them as presence/absence tables.
 BMIInput <- read.table("BugTaxa.csv", header=T, sep=",",as.is=T,skip=0,fill=T,quote="\"",check.names=F,encoding = "UTF-8")
+
+#Standarize date format.
+BMIInput$sampledate <- gsub(" 00:00:00","",BMIInput$sampledate)
+BMIInput$sampledate <- as.Date(BMIInput$sampledate,format="%Y-%m-%d")
+BMIInput$sampledate <- format(BMIInput$sampledate,format="%m/%d/%y")
+#Create unique sample identifier.
+BMIInput$UniqueID <- paste(BMIInput$stationcode,BMIInput$sampledate)
+
+#Filter morphological algal data to containing the same samples as the metagenomic ones.
+BMIInput <- BMIInput[BMIInput$UniqueID %in% sampleIDs$UniqueID,]
 
 #Get a list of unique OTU names from count tables and convert to a data frame.
 uniqueOTUs <- as.data.frame(unique(BMIInput$finalid))
